6066dfd7c93b8083e505bdd1846d9bcb98f94536
4802852 <4802852@gmail.com>
2021-09-07 02:54:10 +0900

    stock analysis display revision

--- stock_analysis/main.py
+++ stock_analysis/main.py
@@ -7,6 +7,7 @@ def stock_analysis(ticker):
     sector = input(
         "0: Sector median 1: Basic Materials, 2: Communication Services, 3: Consumer Cyclical, 4: Consumer Defensive, 5: Energy, 6: Financial, 7: Healthcare, 8: Industials, 9: Real Estate, 10: Technology, 11: Utilities\nSector number: "
     )
+    print()
     time.sleep(0.5)
     get_per(ticker)
     time.sleep(0.2)
